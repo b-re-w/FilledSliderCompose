0bde1ac1a3194bed8efa8c8204f3986be0ac7b1f
BREW <81044362+b-re-w@users.noreply.github.com>
2025-08-20 05:32:39 +0900

    Chore: Build script update

--- filled-slider-compose/build.gradle.kts
+++ filled-slider-compose/build.gradle.kts
@@ -2,27 +2,17 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetpack.compose)
+    alias(libs.plugins.compose.compiler)
 }
 
 @OptIn(org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi::class)
 kotlin {
-    targetHierarchy.default()
+    applyDefaultHierarchyTemplate()
 
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = JavaVersion.VERSION_1_8.toString()
-            }
-        }
-    }
+    androidTarget()
 
-    jvm("desktop") {
-        compilations.all {
-            kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
-        }
-    }
+    jvm("desktop")
 
-    ios()
     listOf(
         iosX64(),
         iosArm64(),
@@ -41,7 +31,6 @@ kotlin {
                 api(compose.foundation)
                 api(compose.materialIconsExtended)
                 api(compose.material3)
-                implementation(libs.skiko.common)
             }
         }
         val commonTest by getting {
@@ -62,22 +51,12 @@ kotlin {
             }
         }
         val desktopTest by getting
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by getting {
-            dependencies {
-            }
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
     }
 }
 
 android {
     namespace = "io.github.seyoungcho2.slider"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
